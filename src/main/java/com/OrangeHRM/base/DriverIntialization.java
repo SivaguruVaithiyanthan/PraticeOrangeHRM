d11f37bd3a1e3c0f13abe6b95f32649f4e3e0838
SIVAGURU Y.V <yvsivaguru143@gmail.com>
2025-11-26 17:16:08 +0530

    New Pratice project initialalized

---
+++ src/main/java/com/OrangeHRM/base/DriverIntialization.java
@@ -0,0 +1,48 @@
+package com.OrangeHRM.base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class DriverIntialization 
+{
+	private static WebDriver driver = null;
+	
+	@BeforeMethod(alwaysRun = true)
+	public static void setUpBrowser(String BrowserName)
+	{
+		if(BrowserName.equalsIgnoreCase("Chrome"))
+		{
+			driver = new ChromeDriver();
+		}
+		else if(BrowserName.equalsIgnoreCase("Edge"))
+		{
+			driver = new EdgeDriver();
+		}
+		else if(BrowserName.equalsIgnoreCase("FireFox"))
+		{
+			driver = new FirefoxDriver();
+		}
+		else
+		{
+			System.out.println("No Driver Found this name : " + BrowserName);
+		}
+		
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();		
+	}
+	
+	@AfterMethod(alwaysRun = true)
+	public void CloseDriver()
+	{
+		driver.quit();
+	}
+	
+	public WebDriver setdriver() 
+	{
+		return driver;
+	}
+}
